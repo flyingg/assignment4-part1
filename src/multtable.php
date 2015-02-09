dbbc8aef5b2cef049b473adaa973ea52c103f3d7
Gerald <galeg@onid.oregonstate.edu>
2015-02-08 20:10:53 -0800

    Almost doen with multtable.php - error checkiing complete. just need to debug the table logic/syntax

--- src/multtable.php
+++ src/multtable.php
@@ -10,46 +10,138 @@ ini_set('display_erros', 'On');
 <title>MultTable - Gale</title>
 </head>
 <body>
-	<br>
-	<p><b>You will need to enter in 4 parameters using the GET in the URL</b></p>
-	<p><BLOCKQUOTE>They need to be entered in the following sequential order in the URL</BLOCKQUOTE></p>
-	<ol>
-		<li></li>
-		<li></li>
-		<li></li>
-		<li></li>
-	</ol>	
-
 <?php
+/***Start of propgram - Check input and keep track of errors***/
+$numberOfErrors = 0;
 
-/*Need 4 parameters 
-- min-multiplicand
-- max-multiplicand
-- min-multiplier
-- max-multiplier
-*/
-$minMultiplicand;
-$maxMultiplicand;
-$minMultiplier;
-$maxMultiplier;
+$casted_minMultiplicand = isset($_GET['min-multiplicand']) ? (int) $_GET['min-multiplicand'] : null;
+$minMultiplicand = $_GET['min-multiplicand'];
+$casted_maxMultiplicand = isset($_GET['max-multiplicand']) ? (int) $_GET['max-multiplicand'] : null;
+$maxMultiplicand = $_GET['max-multiplicand'];
+$casted_minMultiplier = isset($_GET['min-multiplier']) ? (int) $_GET['min-multiplier'] : null;
+$minMultiplier = $_GET['min-multiplier'];
+$casted_maxMultiplier = isset($_GET['max-multiplier']) ? (int) $_GET['max-multiplier'] : null;
+$maxMultiplier = $_GET['max-multiplier'];
 
-
-
-
-
-
-echo '<p><h3>GET variables </h3>
-<p>
-<table border="1">
-<tr>
-<td>key
-<td>Value';
-
-foreach ($_GET as $key => $value) {
-	echo '<tr><td>' . $key . '<td>' . $value;
+//Check input in the URL for the following: min-multiplicand
+if($_GET['min-multiplicand'] == null){
+	echo 'Missing parameter [min-multiplicand].';
+	$numberOfErrors++;
 }
-echo '<table>';
 
+if($minMultiplicand!=(integer)$minMultiplicand || !$casted_minMultiplicand){
+	echo '[min-multiplicand] must be an integer.';
+	$numberOfErrors++;
+}
+
+echo "<br><br>";
+
+//Check input in the URL for the following: max-multiplicand
+if($_GET['max-multiplicand'] == null){
+	echo 'Missing parameter [max-multiplicand].';
+	$numberOfErrors++;
+}
+
+if($maxMultiplicand!=(integer)$maxMultiplicand || !$casted_maxMultiplicand){
+	echo '[max-multiplicand] must be an integer.';
+	$numberOfErrors++;
+}
+
+echo "<br><br>";
+
+//Check input in the URL for the following: min-multiplier
+if($_GET['min-multiplier'] == null){
+	echo 'Missing parameter [min-multiplier].';
+	$numberOfErrors++;
+}
+
+if($minMultiplier!=(integer)$minMultiplier || !$casted_minMultiplier){
+	echo '[min-multiplier] must be an integer.';
+	$numberOfErrors++;
+}
+
+echo "<br><br>";
+
+//Check input in the URL for the following: max-multiplier
+if($_GET['max-multiplier'] == null){
+	echo 'Missing parameter [max-multiplier].';
+	$numberOfErrors++;
+}
+
+if($maxMultiplier!=(integer)$maxMultiplier || !$casted_maxMultiplier){
+	echo '[max-multiplier] must be an integer.';
+	$numberOfErrors++;
+}
+
+$i; 
+$j; 
+$productResult;
+
+if ($minMultiplicand > $maxMultiplicand){
+	echo "Minimum [multiplicand] larger than maximum.";
+	$numberOfErrors++;
+}
+
+if ($minMultiplier > $maxMultiplier){
+	echo "Minimum [multiplier] larger than maximum.";
+	$numberOfErrors++;
+}
+
+
+
+
+if($numberOfErrors > 0){
+	echo "<table>";
+	echo "<caption>PHP MultTable</capation>";
+	echo "<thead>";
+	echo "<tr>";
+	echo "<th></th>";
+	for($i = $minMultiplier; $i <= $maxMultiplier; $i++){
+		echo "<th>$i</th>";
+	}
+	echo "</tr>";
+	echo "</thead>";
+	echo "</tbody>";
+	for ($j = $minMultiplicand; $j <= $maxMultiplicand; $j++){
+		echo "<tr>";
+		echo "<th>$j</th>";
+		for ($i = $minMultiplier; $i <= $casted_maxMultiplier; $i++){
+			$productResult = $i * $j;
+			echo "<td>$productResult</td>";
+		}
+		echo "</tr>";
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+	//echo "";
+
+	//echo "</table>";
+}
+
+
+
+// /***Helper Section to let you know what you've typed in***/
+// echo '<p><h3>Your Entered parameters</h3>
+// <p>
+// <table border="1">
+// <tr>
+// <td>key
+// <td>Value';
+
+// foreach ($_GET as $key => $value) {
+// 	echo '<tr><td>' . $key . '<td>' . $value;
+// }
+// echo '<table>';
 ?>
 
 </body>
