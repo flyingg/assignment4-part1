eec680304726ce830843ba88ed0db48de22432e8
Gerald <galeg@onid.oregonstate.edu>
2015-02-10 21:51:26 -0800

    Almost done with loopback.ph

--- src/loopback.php
+++ src/loopback.php
@@ -1,6 +1,38 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_erros', 'On');
-
-	echo "HI, im PHP yo"
 ?>
+
+<!DOCTYPE html>
+<html lang = "en">
+<head>
+<meta charset = "utf-8">
+<title>LoopBack - Gale</title>
+</head>
+<body>
+
+<?php
+
+$getVariable = $_GET['hello'];
+
+echo $getVariable;
+
+echo "<br>I just wanted to say a few thing to you";
+
+
+/***Helper Section to let you know what you've typed in***/
+echo "<br><br>";
+echo '<p><h3>Your Entered parameters</h3>
+<p>
+<table border="1">
+<tr>
+<td>key
+<td>Value';
+
+foreach ($_GET as $key => $value) {
+	echo '<tr><td>' . $key . '<td>' . $value;
+}
+echo '<table>';
+echo '</table>';
+?>
+
